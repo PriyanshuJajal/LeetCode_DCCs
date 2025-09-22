a5ea013369f6e770dafb384a06e63a63325a2ebd
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2025-09-22 07:25:18 +0530

    Efficient C++/Java Solution For Day-22 DCC

---
+++ September/Day22/CntElsWithMaxFreq.java
@@ -0,0 +1,56 @@
+// LEETCODE 3005. Count Elements With Maximum Frequency
+
+// You are given an array nums consisting of positive integers.
+
+// Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+// The frequency of an element is the number of occurrences of that element in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,2,3,1,4]
+// Output: 4
+// Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+// So the number of elements in the array with maximum frequency is 4.
+// Example 2:
+
+// Input: nums = [1,2,3,4,5]
+// Output: 5
+// Explanation: All elements of the array have a frequency of 1 which is the maximum.
+// So the number of elements in the array with maximum frequency is 5.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+
+// 🚀 Approach
+// Use a fixed-size frequency array f[101] to count occurrences of each number (assuming values are in the range 1–100).
+// Traverse the frequency array to find the maximum frequency maxCnt.
+// Traverse again to sum up all occurrences of elements that appear exactly maxCnt times.
+// Return the total count.
+
+
+
+
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int[] f = new int[101];
+        for (int num : nums) f[num]++;
+        
+        int maxCnt = 0;
+        for (int cnt : f) {
+            maxCnt = Math.max(maxCnt , cnt);
+        }
+
+        int res = 0;
+        for (int val : f) {
+            if (val == maxCnt) res += maxCnt;
+        }
+        return res;
+    }
+}
