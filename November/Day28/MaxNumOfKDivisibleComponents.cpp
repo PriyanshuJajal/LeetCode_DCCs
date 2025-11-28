ecec672587ad1fd6cfc5e3d3435e8313c9eccdd9
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2025-11-28 15:13:14 +0530

    Efficient C++/Java Solution For Day-28 DCC

---
+++ November/Day28/MaxNumOfKDivisibleComponents.cpp
@@ -0,0 +1,112 @@
+// LEETCODE 2872. Maximum Number of K-Divisible Components
+
+// There is an undirected tree with n nodes labeled from 0 to n - 1. You are given the integer n and a 2D integer array edges of length n - 1, where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
+
+// You are also given a 0-indexed integer array values of length n, where values[i] is the value associated with the ith node, and an integer k.
+
+// A valid split of the tree is obtained by removing any set of edges, possibly empty, from the tree such that the resulting components all have values that are divisible by k, where the value of a connected component is the sum of the values of its nodes.
+
+// Return the maximum number of components in any valid split.
+
+ 
+
+// Example 1:
+
+
+// Input: n = 5, edges = [[0,2],[1,2],[1,3],[2,4]], values = [1,8,1,4,4], k = 6
+// Output: 2
+// Explanation: We remove the edge connecting node 1 with 2. The resulting split is valid because:
+// - The value of the component containing nodes 1 and 3 is values[1] + values[3] = 12.
+// - The value of the component containing nodes 0, 2, and 4 is values[0] + values[2] + values[4] = 6.
+// It can be shown that no other valid split has more than 2 connected components.
+// Example 2:
+
+
+// Input: n = 7, edges = [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6]], values = [3,0,6,1,5,2,1], k = 3
+// Output: 3
+// Explanation: We remove the edge connecting node 0 with 2, and the edge connecting node 0 with 1. The resulting split is valid because:
+// - The value of the component containing node 0 is values[0] = 3.
+// - The value of the component containing nodes 2, 5, and 6 is values[2] + values[5] + values[6] = 9.
+// - The value of the component containing nodes 1, 3, and 4 is values[1] + values[3] + values[4] = 6.
+// It can be shown that no other valid split has more than 3 connected components.
+ 
+
+// Constraints:
+
+// 1 <= n <= 3 * 104
+// edges.length == n - 1
+// edges[i].length == 2
+// 0 <= ai, bi < n
+// values.length == n
+// 0 <= values[i] <= 109
+// 1 <= k <= 109
+// Sum of values is divisible by k.
+// The input is generated such that edges represents a valid tree.
+
+
+
+
+// 🚀 Approach
+// Convert the input edges into an adjacency list representation of the tree.
+// Perform a DFS starting from node 0.
+// At each node:
+//     Compute the sum of values in its subtree by recursively visiting children.
+//     If the subtree sum is divisible by k, increment the component counter and return 0 (since this subtree can be cut off as a separate component).
+//     Otherwise, return the subtree sum to the parent.
+// Finally, the counter gives the maximum number of k-divisible components possible.
+
+
+// 🕛 Complexity
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+private:
+    long long compCnt = 0;
+
+    long long dfs(int u , int p , vector<long long>& vals , long long k , vector<vector<int>>& adj) {
+        long long currSum = vals[u];
+        for (int v : adj[u]) {
+            if (v != p) {
+                currSum = (currSum + dfs(v , u , vals , k , adj));
+            }
+        }
+
+        if (currSum % k == 0) {
+            compCnt++;   
+            return 0;
+        }
+        else return currSum;
+    }
+
+public:
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        // compCnt = 0;
+        long long K = (long long)k;
+        vector<long long> vals(n);
+        for (int i = 0; i < n; i++) vals[i] = values[i];
+
+        vector<vector<int>> adj(n);
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        dfs(0 , -1 , vals , K , adj);
+
+        return compCnt;
+    }
+};
+
+
