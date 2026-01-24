f188d09fe3d6a83cc94f54aa8496cc09b8a2631b
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-01-24 09:23:58 +0530

    Efficient C++/Java Solution For Day-24 DCC

---
+++ January26/Day24/MinimizeMaxPairSumInArr.cpp
@@ -0,0 +1,74 @@
+// LEETCODE 1877. Minimize Maximum Pair Sum in Array
+
+// The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+// For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+// Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+// Each element of nums is in exactly one pair, and
+// The maximum pair sum is minimized.
+// Return the minimized maximum pair sum after optimally pairing up the elements.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,5,2,3]
+// Output: 7
+// Explanation: The elements can be paired up into pairs (3,3) and (5,2).
+// The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
+// Example 2:
+
+// Input: nums = [3,5,4,2,4,6]
+// Output: 8
+// Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
+// The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8.
+ 
+
+// Constraints:
+
+// n == nums.length
+// 2 <= n <= 105
+// n is even.
+// 1 <= nums[i] <= 105
+
+
+// 🚀 Approach
+// Sort the array in non‑decreasing order.
+// Use two pointers:
+//     One starting at the beginning (i = 0)
+//     One at the end (j = n - 1)
+// Form pairs (nums[i] , nums[j]) and compute their sum.
+// Track the maximum sum among all pairs.
+// Move both pointers inward (i++, j--) until all pairs are formed.
+// Return the maximum sum found — this is the minimized maximum pair sum.
+
+// 🕛 Complexity
+// Time complexity: O(nlogn)
+// Space complexity: O(1)
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(begin(nums) , end(nums));
+        int n = nums.size();
+        int maxSum = 0;
+        int i = 0 , j = n - 1;
+
+        while(i < j) {
+            int sum = nums[i] + nums[j];
+            maxSum = max(maxSum , sum);
+            i++;
+            j--;
+        }
+        return maxSum;
+    }
+};
