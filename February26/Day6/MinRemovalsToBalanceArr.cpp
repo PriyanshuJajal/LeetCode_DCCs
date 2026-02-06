3ccfa5d0f5a74535b5b46cef11c74df044339dcf
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-02-06 14:35:26 +0530

    Efficient C++/Java Solution For Day-6 DCC

---
+++ February26/Day6/MinRemovalsToBalanceArr.cpp
@@ -0,0 +1,91 @@
+// LEETCODE 3634. Minimum Removals to Balance Array
+
+// You are given an integer array nums and an integer k.
+
+// An array is considered balanced if the value of its maximum element is at most k times the minimum element.
+
+// You may remove any number of elements from nums​​​​​​​ without making it empty.
+
+// Return the minimum number of elements to remove so that the remaining array is balanced.
+
+// Note: An array of size 1 is considered balanced as its maximum and minimum are equal, and the condition always holds true.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,1,5], k = 2
+
+// Output: 1
+
+// Explanation:
+
+// Remove nums[2] = 5 to get nums = [2, 1].
+// Now max = 2, min = 1 and max <= min * k as 2 <= 1 * 2. Thus, the answer is 1.
+// Example 2:
+
+// Input: nums = [1,6,2,9], k = 3
+
+// Output: 2
+
+// Explanation:
+
+// Remove nums[0] = 1 and nums[3] = 9 to get nums = [6, 2].
+// Now max = 6, min = 2 and max <= min * k as 6 <= 2 * 3. Thus, the answer is 2.
+// Example 3:
+
+// Input: nums = [4,6], k = 2
+
+// Output: 0
+
+// Explanation:
+
+// Since nums is already balanced as 6 <= 4 * 2, no elements need to be removed.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 109
+// 1 <= k <= 105
+
+
+// 🚀 Approach
+// Sort the array: Sorting ensures that the minimum and maximum of any subarray are at the ends.
+// Two-pointer technique:
+//     Fix the left pointer i.
+//     Move the right pointer j as far as possible while the condition nums[j] <= nums[i] * k holds.
+//     This gives us a valid subarray from i to j - 1.
+// Calculate removals:
+//     The size of this valid subarray is (j - i).
+//     To make the whole array valid, we would need to remove the rest: n - (j - i).
+//     Track the minimum removals across all possible windows.
+// The minimum number of removals required is stored in minRem.
+
+// 🕛 Complexity
+// Time complexity: O(nlogn)
+// Space complexity: O(1)
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(begin(nums) , end(nums));
+
+        int minRem = n - 1;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while(j < n && nums[j] <= (long long)nums[i] * k) j++;
+            minRem = min(minRem , n - (j - i));
+        }
+        return minRem;
+    }
+};
