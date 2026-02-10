b8685a10e7b8f64f31a9d5b60f5d06ebd9700b07
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-02-10 17:39:10 +0530

    Efficient C++/Java Solution For Day-10 DCC

---
+++ February26/Day10/LongestBalancedSubArr1.cpp
@@ -0,0 +1,92 @@
+// LEETCODE 3719. Longest Balanced Subarray I
+
+// You are given an integer array nums.
+
+// A subarray is called balanced if the number of distinct even numbers in the subarray is equal to the number of distinct odd numbers.
+
+// Return the length of the longest balanced subarray.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,5,4,3]
+
+// Output: 4
+
+// Explanation:
+
+// The longest balanced subarray is [2, 5, 4, 3].
+// It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [5, 3]. Thus, the answer is 4.
+// Example 2:
+
+// Input: nums = [3,2,2,5,4]
+
+// Output: 5
+
+// Explanation:
+
+// The longest balanced subarray is [3, 2, 2, 5, 4].
+// It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [3, 5]. Thus, the answer is 5.
+// Example 3:
+
+// Input: nums = [1,2,3,2]
+
+// Output: 3
+
+// Explanation:
+
+// The longest balanced subarray is [2, 3, 2].
+// It has 1 distinct even number [2] and 1 distinct odd number [3]. Thus, the answer is 3.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1500
+// 1 <= nums[i] <= 105
+ 
+
+// 🚀 Approach
+// Use two nested loops to explore all subarrays:
+//     Outer loop i marks the start of the subarray.
+//     Inner loop j marks the end of the subarray.
+// For each subarray:
+//     Use two Sets to track distinct even and odd numbers.
+//     If nums[j] is even, add to even set; else add to odd set.
+//     If the sizes of both sets match, update maxLen with the current subarray length.
+
+// This brute-force approach ensures all possible subarrays are checked.
+
+// 🕛 Complexity
+// Time complexity: O(n²)
+// Space complexity: O(n)
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> even , odd;
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            even.clear();
+            odd.clear();
+
+            for (int j = i; j < n; j++) {
+                if ((nums[j] & 1) == 0) even.insert(nums[j]);
+                else odd.insert(nums[j]);
+                
+                if (even.size() == odd.size()) maxLen = max(maxLen , j - i + 1);
+            }
+        }
+        return maxLen;
+    }
+};
