4ef4781c4ffeb3b9057ccafe578e8a00a9b9d9a9
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-04-25 17:40:23 +0530

    Efficient C++ Solution For Day-25 DCC

---
+++ April26/Day25/MaximizeDistBetweenPtsOnASquare.cpp
@@ -0,0 +1,120 @@
+// LEETCODE 3464. Maximize the Distance Between Points on a Square
+
+// You are given an integer side, representing the edge length of a square with corners at (0, 0), (0, side), (side, 0), and (side, side) on a Cartesian plane.
+
+// You are also given a positive integer k and a 2D integer array points, where points[i] = [xi, yi] represents the coordinate of a point lying on the boundary of the square.
+
+// You need to select k elements among points such that the minimum Manhattan distance between any two points is maximized.
+
+// Return the maximum possible minimum Manhattan distance between the selected k points.
+
+// The Manhattan Distance between two cells (xi, yi) and (xj, yj) is |xi - xj| + |yi - yj|.
+
+ 
+
+// Example 1:
+
+// Input: side = 2, points = [[0,2],[2,0],[2,2],[0,0]], k = 4
+
+// Output: 2
+
+// Explanation:
+
+
+
+// Select all four points.
+
+// Example 2:
+
+// Input: side = 2, points = [[0,0],[1,2],[2,0],[2,2],[2,1]], k = 4
+
+// Output: 1
+
+// Explanation:
+
+
+
+// Select the points (0, 0), (2, 0), (2, 2), and (2, 1).
+
+// Example 3:
+
+// Input: side = 2, points = [[0,0],[0,1],[0,2],[1,2],[2,0],[2,2],[2,1]], k = 5
+
+// Output: 1
+
+// Explanation:
+
+
+// Select the points (0, 0), (0, 1), (0, 2), (1, 2), and (2, 2).
+
+
+// Constraints:
+
+// 1 <= side <= 109
+// 4 <= points.length <= min(4 * side, 15 * 103)
+// points[i] == [xi, yi]
+// The input is generated such that:
+// points[i] lies on the boundary of the square.
+// All points[i] are unique.
+// 4 <= k <= min(25, points.length)
+
+
+// Time Complexity: O(nk * log(side) * logn)
+// Space Complexity: O(n)
+
+
+class Solution {
+public:
+    int maxDistance(int side, vector<vector<int>>& points, int k) {
+        vector<long long> arr;
+        for (auto& p : points) {
+            int x = p[0];
+            int y = p[1];
+
+            if (x == 0) arr.push_back(y);
+            else if (y == side) {
+                arr.push_back(side + x);
+            }
+            else if (x == side) {
+                arr.push_back(side * 3LL - y);
+            }
+            else {
+                arr.push_back(side * 4LL - x);
+            }
+        }
+
+        sort(begin(arr) , end(arr));
+
+        auto check = [&](long long limit) -> bool {
+            for (long long st : arr) {
+                long long end = st + side * 4LL - limit;
+                long long curr = st;
+
+                for (int i = 0; i < k - 1; i++) {
+                    auto it = ranges::lower_bound(arr , curr + limit);
+                    if (it == arr.end() || *it > end) {
+                        curr = -1;
+                        break;
+                    }
+                    curr = *it;
+                }
+
+                if (curr >= 0) return true;
+            } 
+            return false;
+        };
+
+        long long l = 1 , h = side;
+        int dist = 0;
+        while(l <= h) {
+            long long mid = l + (h - l) / 2;
+            if (check(mid)) {
+                l = mid + 1;
+                dist = mid;
+            }
+            else h = mid - 1;
+        }
+        
+        return dist;
+    }
+};
