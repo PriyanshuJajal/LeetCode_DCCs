3160ee6676426c4f3e7852dd6b531eb1b148c42a
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-05-11 08:46:08 +0530

    Efficient C++ Solution For Day-11 DCC

---
+++ May26/Day11/SeparateTheDigitsInAnArray.cpp
@@ -0,0 +1,66 @@
+// LEETCODE 2553. Separate the Digits in an Array
+
+// Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+// To separate the digits of an integer is to get all the digits it has in the same order.
+
+// For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+ 
+
+// Example 1:
+
+// Input: nums = [13,25,83,77]
+// Output: [1,3,2,5,8,3,7,7]
+// Explanation: 
+// - The separation of 13 is [1,3].
+// - The separation of 25 is [2,5].
+// - The separation of 83 is [8,3].
+// - The separation of 77 is [7,7].
+// answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.
+// Example 2:
+
+// Input: nums = [7,1,3,9]
+// Output: [7,1,3,9]
+// Explanation: The separation of each integer in nums is itself.
+// answer = [7,1,3,9].
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// 1 <= nums[i] <= 105
+
+
+// 🚀 Approach
+// Iterate over the array nums from the last element to the first.
+// For each number:
+//     Use % 10 to extract the last digit.
+//     Use / 10 to remove that digit.
+//     Push each digit into the result vector.
+// After processing all numbers, reverse the result vector to restore the correct order.
+
+// 🕛 Complexity
+// Time complexity: O(nlogm)
+// n = length of nums
+// m = max value in nums
+
+// Space complexity: O(1) (except result vector)
+
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> res;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int num = nums[i];
+
+            while(num > 0) {
+                res.push_back(num % 10);
+                num /= 10;
+            }
+        }
+
+        reverse(begin(res) , end(res));
+        return res;
+    }
+};
