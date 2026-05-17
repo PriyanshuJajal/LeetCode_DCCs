2a5cc5a4065dcb6f8c6c7fe434c4491d303e25fe
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-05-17 09:05:03 +0530

    Efficient C++ Solution For Day-17 DCC

---
+++ May26/Day17/JumpGameⅢ.cpp
@@ -0,0 +1,71 @@
+// LEETCODE 1306. Jump Game III
+
+// Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+
+// Notice that you can not jump outside of the array at any time.
+
+ 
+
+// Example 1:
+
+// Input: arr = [4,2,3,0,3,1,2], start = 5
+// Output: true
+// Explanation: 
+// All possible ways to reach at index 3 with value 0 are: 
+// index 5 -> index 4 -> index 1 -> index 3 
+// index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+// Example 2:
+
+// Input: arr = [4,2,3,0,3,1,2], start = 0
+// Output: true 
+// Explanation: 
+// One possible way to reach at index 3 with value 0 is: 
+// index 0 -> index 4 -> index 1 -> index 3
+// Example 3:
+
+// Input: arr = [3,0,2,1,2], start = 2
+// Output: false
+// Explanation: There is no way to reach at index 1 with value 0.
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 5 * 104
+// 0 <= arr[i] < arr.length
+// 0 <= start < arr.length
+
+
+// 🚀 Approach
+// Start DFS from the given start index and explore possible jumps.
+// At each index, check base conditions: out of bounds, already visited, or value 0.
+// If the value is 0, return true immediately (goal reached).
+// Mark the current index as visited by negating its value to avoid revisiting.
+// Recursively try both directions: i - arr[i] and i + arr[i].
+// Return true if either recursive call leads to a 0, otherwise false.
+
+// 🕛 Complexity
+// Time complexity: O(n)
+// Space complexity: O(n) (Recursive stack space)
+
+
+
+class Solution {
+public:
+    int n;
+    bool dfs(vector<int>& arr , int i) {
+        if (i < 0 || i >= n || arr[i] < 0) return false;
+        if (arr[i] == 0) return true;
+
+        arr[i] *= -1;
+
+        int l = dfs(arr , i - arr[i]);
+        int r = dfs(arr , i + arr[i]);
+
+        return l || r;
+    }
+
+    bool canReach(vector<int>& arr, int start) {
+        n = arr.size();
+        return dfs(arr , start);
+    }
+};
