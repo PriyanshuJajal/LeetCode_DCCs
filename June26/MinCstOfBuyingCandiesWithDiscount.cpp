7e293e2d0029c468fc54448a3010cc9a3f0611d5
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-06-01 11:40:21 +0530

    Efficient C++ Solution For Day-1 DCC

---
+++ June26/MinCstOfBuyingCandiesWithDiscount.cpp
@@ -0,0 +1,71 @@
+// LEETCODE 2144. Minimum Cost of Buying Candies With Discount
+
+// A shop is selling candies at a discount. For every two candies sold, the shop gives a third candy for free.
+
+// The customer can choose any candy to take away for free as long as the cost of the chosen candy is less than or equal to the minimum cost of the two candies bought.
+
+// For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 2 and 3, they can take the candy with cost 1 for free, but not the candy with cost 4.
+// Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum cost of buying all the candies.
+
+ 
+
+// Example 1:
+
+// Input: cost = [1,2,3]
+// Output: 5
+// Explanation: We buy the candies with costs 2 and 3, and take the candy with cost 1 for free.
+// The total cost of buying all candies is 2 + 3 = 5. This is the only way we can buy the candies.
+// Note that we cannot buy candies with costs 1 and 3, and then take the candy with cost 2 for free.
+// The cost of the free candy has to be less than or equal to the minimum cost of the purchased candies.
+// Example 2:
+
+// Input: cost = [6,5,7,9,2,2]
+// Output: 23
+// Explanation: The way in which we can get the minimum cost is described below:
+// - Buy candies with costs 9 and 7
+// - Take the candy with cost 6 for free
+// - We buy candies with costs 5 and 2
+// - Take the last remaining candy with cost 2 for free
+// Hence, the minimum cost to buy all candies is 9 + 7 + 5 + 2 = 23.
+// Example 3:
+
+// Input: cost = [5,5]
+// Output: 10
+// Explanation: Since there are only 2 candies, we buy both of them. There is not a third candy we can take for free.
+// Hence, the minimum cost to buy all candies is 5 + 5 = 10.
+ 
+
+// Constraints:
+
+// 1 <= cost.length <= 100
+// 1 <= cost[i] <= 100
+
+
+// 🧠 Intuition
+// When buying candies with a “buy 2, get 1 free” offer, the idea is simple: sort prices in descending order so that the free candy is always the cheapest among every group of three. This ensures we minimize total cost.
+
+// 🚀 Approach
+// Sort the array in descending order.
+// Traverse the list, and for every group of three candies, skip the 3rd one (since it’s free).
+// Add the cost of the other two to the total.
+// Return the accumulated minimum cost.
+
+// 🕛 Complexity
+// Time Complexity: O(nlogn) 
+// Space Complexity: O(1)
+
+
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(begin(cost) , end(cost) , greater<int>());
+        int minCst = 0;
+
+        for (int i = 0; i < cost.size(); i++) {
+            if (i % 3 != 2) {
+                minCst += cost[i];
+            }
+        }
+        return minCst;
+    }
+};
