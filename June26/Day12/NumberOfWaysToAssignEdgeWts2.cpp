10613e45c9ea93b98c79caf176270fe6ecb445d5
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-06-12 09:09:54 +0530

    Efficient C++ Solution For Day-12 DCC

---
+++ June26/Day12/NumberOfWaysToAssignEdgeWts2.cpp
@@ -0,0 +1,161 @@
+// LEETCODE 3559. Number of Ways to Assign Edge Weights II
+
+// There is an undirected tree with n nodes labeled from 1 to n, rooted at node 1. The tree is represented by a 2D integer array edges of length n - 1, where edges[i] = [ui, vi] indicates that there is an edge between nodes ui and vi.
+
+// Initially, all edges have a weight of 0. You must assign each edge a weight of either 1 or 2.
+
+// The cost of a path between any two nodes u and v is the total weight of all edges in the path connecting them.
+
+// You are given a 2D integer array queries. For each queries[i] = [ui, vi], determine the number of ways to assign weights to edges in the path such that the cost of the path between ui and vi is odd.
+
+// Return an array answer, where answer[i] is the number of valid assignments for queries[i].
+
+// Since the answer may be large, apply modulo 109 + 7 to each answer[i].
+
+// Note: For each query, disregard all edges not in the path between node ui and vi.
+
+ 
+
+// Example 1:
+
+
+
+// Input: edges = [[1,2]], queries = [[1,1],[1,2]]
+
+// Output: [0,1]
+
+// Explanation:
+
+// Query [1,1]: The path from Node 1 to itself consists of no edges, so the cost is 0. Thus, the number of valid assignments is 0.
+// Query [1,2]: The path from Node 1 to Node 2 consists of one edge (1 → 2). Assigning weight 1 makes the cost odd, while 2 makes it even. Thus, the number of valid assignments is 1.
+// Example 2:
+
+
+
+// Input: edges = [[1,2],[1,3],[3,4],[3,5]], queries = [[1,4],[3,4],[2,5]]
+
+// Output: [2,1,4]
+
+// Explanation:
+
+// Query [1,4]: The path from Node 1 to Node 4 consists of two edges (1 → 3 and 3 → 4). Assigning weights (1,2) or (2,1) results in an odd cost. Thus, the number of valid assignments is 2.
+// Query [3,4]: The path from Node 3 to Node 4 consists of one edge (3 → 4). Assigning weight 1 makes the cost odd, while 2 makes it even. Thus, the number of valid assignments is 1.
+// Query [2,5]: The path from Node 2 to Node 5 consists of three edges (2 → 1, 1 → 3, and 3 → 5). Assigning (1,2,2), (2,1,2), (2,2,1), or (1,1,1) makes the cost odd. Thus, the number of valid assignments is 4.
+ 
+
+// Constraints:
+
+// 2 <= n <= 105
+// edges.length == n - 1
+// edges[i] == [ui, vi]
+// 1 <= queries.length <= 105
+// queries[i] == [ui, vi]
+// 1 <= ui, vi <= n
+// edges represents a valid tree.
+
+// Time Complexity: O(nlogn + mlogn)
+// Space Complexity: O(nlogn)
+
+
+
+class LCA {
+public:
+    LCA(const vector<vector<int>>& edges , const int root = 1) {
+        n = edges.size() + 1;
+        m = (log(n) / log(2)) + 1;
+        e.resize(n + 1);
+        d.resize(n + 1);
+        f.resize(n + 1 , vector<int>(m , 0));
+
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            e[u].push_back(v);
+            e[v].push_back(u);
+        }
+
+        dfs(root , 0);
+        for (int i = 1; i < m; i++) {
+            for (int x = 1; x <= n; x++) {
+                f[x][i] = f[f[x][i - 1]][i - 1];
+            }
+        }
+    }
+
+    void dfs(int x , int prev) {
+        f[x][0] = prev;
+        for (auto& y : e[x]) {
+            if (y == prev) continue;
+
+            d[y] = d[x] + 1;
+            dfs(y , x);
+        }
+    }
+
+    int lca(int x , int y) {
+        if (d[x] > d[y]) {
+            swap(x , y);
+        }
+        
+        for (int i = m - 1; i >= 0; i--) {
+            if (d[x] <= d[f[y][i]]) {
+                y = f[y][i];
+            }
+        }
+
+        if (x == y) return x;
+
+        for (int i = m - 1; i >= 0; i--) {
+            if (f[y][i] != f[x][i]) {
+                x = f[x][i];
+                y = f[y][i];
+            }
+        }
+
+        return f[x][0];
+    }
+
+    int dist(int x , int y) {
+        return d[x] + d[y] - d[lca(x , y)] * 2;
+    }
+
+private:
+    int n;
+    int m;
+    vector<int> d;
+    vector<vector<int>> e;
+    vector<vector<int>> f;
+};
+
+const int MOD = 1e9 + 7;
+const int N = 100010;
+int p2[N];
+
+auto init = [] {
+    p2[0] = 1;
+    for (int i = 1; i < N; i++) {
+        p2[i] = p2[i - 1] * 2 % MOD;
+    }
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<int> assignEdgeWeights(vector<vector<int>>& edges, vector<vector<int>>& queries) {
+        LCA lca(edges , 1);
+        int m = queries.size();
+        vector<int> res(m);
+
+        for (int i = 0; i < m; i++) {
+            int x = queries[i][0];
+            int y = queries[i][1];
+
+            if (x != y) {
+                res[i] = p2[lca.dist(x , y) - 1];
+            }
+        }
+
+        return res;
+    }
+};
