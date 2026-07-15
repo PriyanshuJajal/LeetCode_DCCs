91dda84ea7b1b9c864316fcef5fcad80e915ee16
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2026-07-15 06:44:53 +0530

    Efficient C++ Solution For Day-15 DCC

---
+++ July26/Day15/GCDOfOdd&EvenSums.cpp
@@ -0,0 +1,13 @@
+// LEETCODE 3658. GCD of Odd and Even Sums
+
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
